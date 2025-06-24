897729c890e7c42cee6d9dbef6fb13c3b370dd97
Gordon <glie0003@student.monash.edu>
2025-06-24 15:13:34 +1000

    back to original main

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
-resource "aws_instance" "demo-server1" {
-    # instance config
+resource "aws_instance" "ec2-instance1" {
+    ami = "ami-010045ee1d3ea738b"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "demo-server1"
+    }
 }
